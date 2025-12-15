86fe8f4b8d295c7d3203ddcacc69c14f09100b91
naufal-dd <kamisjhir@gmail.com>
2025-12-15 16:01:33 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
   "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
-var menuItemsTitleHtml = "snippets/menu-items-title-snippet.html";
-var menuItemHtml = "snippets/menu-item-snippet.html";
+var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemHtml = "snippets/menu-item.html";
 
 
 // Convenience function for inserting innerHTML for 'select'
